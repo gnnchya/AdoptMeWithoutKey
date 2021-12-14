606b10958cb13040bff60151afdb138b568e91c9
pondnarawich <pondnarawich@gmail.com>
2021-12-14 20:59:52 +0700

    -

--- post/main.go
+++ post/main.go
@@ -13,7 +13,7 @@ import (
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://10.0.20.204:3000/")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://10.0.0.204:3000/")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
